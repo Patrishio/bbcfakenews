df88e049b8fdddfb5ed5e674894e88e5a46782c2
patrishio <patryk.orlik@gmail.com>
2019-03-02 13:42:43 +0000

    first commit - read rss

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home    
+    require 'rss'
+    
+    # ---------------- 
+    # if app become complex it can be move to the service PORO object
+    feed = RSS::Parser.parse(open('http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml').read, false)    
+    @items = feed.items
+  end
+end
